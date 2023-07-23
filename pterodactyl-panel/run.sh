7d59593fc0d578a54937ee48174849f53f267140
DDanii <ddani1995@gmail.com>
2023-07-23 22:23:28 +0000

    added DB_HOST logging

--- pterodactyl-panel/run.sh
+++ pterodactyl-panel/run.sh
@@ -4,9 +4,13 @@ export SSL_CERT=/ssl/$(bashio::config 'certfile')
 export SSL_CERT_KEY=/ssl/$(bashio::config 'keyfile')
 
 DB_HOST=$(bashio::services "mysql" "host")
+
 if [ $DB_HOST = "localhost" ]; then
 	DB_HOST=127.0.0.1
 fi
+
+echo "DB_HOST="$DB_HOST
+
 export DB_HOST=$DB_HOST
 export DB_PASSWORD=$(bashio::services "mysql" "password")
 export DB_PORT=$(bashio::services "mysql" "port")
