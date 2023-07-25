c7655158a3067540460a27d73a2749f320898f3e
DDanii <ddani1995@gmail.com>
2023-07-25 06:16:27 +0200

    Update run.sh

--- pterodactyl-panel/run.sh
+++ pterodactyl-panel/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bashio
 
+sed s_#!/bin/sh_#!/bin/sh/necho $(env)_
+
 export SSL_CERT=/ssl/$(bashio::config 'certfile')
 export SSL_CERT_KEY=/ssl/$(bashio::config 'keyfile')
 
@@ -11,7 +13,7 @@ fi
 
 
 export DB_HOST=$DB_HOST
-echo env
+echo $(env)
 export DB_PASSWORD=$(bashio::services "mysql" "password")
 export DB_PORT=$(bashio::services "mysql" "port")
 export DB_USERNAME=$(bashio::services "mysql" "username")
