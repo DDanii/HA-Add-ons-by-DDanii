f2a7f12e5bd8bf7a4c6dec678d361f7a118d3217
DDanii <ddani1995@gmail.com>
2023-07-26 23:40:24 +0000

    added user creation and restored ssl setting

--- pterodactyl-panel/run.sh
+++ pterodactyl-panel/run.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/bashio
 redis-server &
-sed 's_/bin/sh_/bin/sh\necho $(env)_' </entrypoint.sh >/tmp.sh
-cat /tmp.sh > /entrypoint.sh
-sed 's_/bin/sh_/bin/sh\necho $(env)_' </usr/local/bin/wait-for >/tmp.sh
-cat /tmp.sh > /usr/local/bin/wait-for
-
-rm /tmp.sh
-
-export SSL_CERT=/ssl/$(bashio::config 'certfile')
-export SSL_CERT_KEY=/ssl/$(bashio::config 'keyfile')
-
-DB_HOST=$(bashio::services "mysql" "host")
-
-if [ $DB_HOST = "localhost" ]; then
-	DB_HOST=127.0.0.1
-fi
 
 
-export DB_HOST=$DB_HOST
+export DB_HOST=$(bashio::services "mysql" "host")
 export DB_PASSWORD=$(bashio::services "mysql" "password")
 export DB_PORT=$(bashio::services "mysql" "port")
 export DB_USERNAME=$(bashio::services "mysql" "username")
 export DB_PORT=3306
 export DB_DATABASE=pterodactyl
-echo $(env)
+
+setup_user=false
+
+if [ ! -d /config/addons_configs/pterodactyl-panel ]; then
+    echo "Creating /config/addons_configs/pterodactyl-panel"
+    mkdir -p /config/addons_configs/pterodactyl-panel
+    setup_user=true
+fi
+
+ln -s /config/addons_configs/pterodactyl-panel /data/
+
+if bashio::config.true 'ssl'; then
+    export SSL_CERT=/ssl/$(bashio::config 'certfile')
+    export SSL_CERT_KEY=/ssl/$(bashio::config 'keyfile')
+fi
+
+
+if [ $setup_user = "true" ]; then
+	echo "[setup] creating root user with password homeassistant..."
+    php artisan p:user:make --email=root@example.com --username=root --name-first=ro --name-last=ot --admin=1 --password=homeassistant
+fi
 
 echo "[start] Starting Pterodactyl Panel"
 
