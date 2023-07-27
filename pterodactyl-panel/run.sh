e4d8390111af066ae53bb298a57fbc987c87347b
DDanii <ddani1995@gmail.com>
2023-07-27 06:49:52 +0000

    add user delete

--- pterodactyl-panel/run.sh
+++ pterodactyl-panel/run.sh
@@ -12,6 +12,9 @@ export DB_DATABASE=pterodactyl
 setup_user=false
 
 directory=/share/pterodactyl
+
+echo $directory
+
 if [ ! -d $directory ]; then
     echo "Creating ${directory}"
     mkdir -p $directory
@@ -26,10 +29,12 @@ if bashio::config.true 'ssl'; then
 fi
 
 
-# if [ $setup_user = "true" ]; then
-# 	echo "[setup] creating root user with password homeassistant..."
-#     php artisan p:user:make --email=root@example.com --username=root --name-first=ro --name-last=ot --admin=1 --password=homeassistant
-# fi
+if [ $setup_user = "true" ]; then
+	echo "[setup] deleting admin user if exist..."
+    php artisan p:user:delete --user=admin -n -q
+	echo "[setup] creating admin user with password homeassistant..."
+    php artisan p:user:make --email=admin@example.com --username=admin --name-first=admin --name-last=admin --admin=1 --password=homeassistant
+fi
 
 echo "[start] Starting Pterodactyl Panel"
 
