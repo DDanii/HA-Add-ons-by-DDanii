8c6e9d99af230476cf9ad907f4db4b442e4d0df3
DDanii <ddani1995@gmail.com>
2023-11-09 15:38:29 +0000

    fix paths

--- MongoDB44/rootfs/run.sh
+++ MongoDB44/rootfs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /config/addons_config/mongodb44
-ln -s /data/db /config/addons_config/mongodb44
+mkdir -p /config/addons_config/mongodb44/db
+ln -s /config/addons_config/mongodb44/db /data/db
 
-/docker-entrypoint.sh mongod
+/usr/local/bin/docker-entrypoint.sh mongod
