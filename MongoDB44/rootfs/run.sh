60735a6196d8448b524c04e69b7c00c05f8dbbe3
DDanii <ddani1995@gmail.com>
2023-11-14 00:42:28 +0000

    change db location test2

--- MongoDB44/rootfs/run.sh
+++ MongoDB44/rootfs/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -d /config/addons/mongodb44/db ] || [ ! -d /config/addons/mongodb44/configdb ]; then
+    mkdir -p /config/addons/mongodb44/db
+    mkdir -p /config/addons/mongodb44/configdb
+fi
+chown -R mongodb:mongodb /config/addons/mongodb44/db /config/addons/mongodb44/configdb
+usermod -d /config/addons/mongodb44/db mongodb
 
+EXPORT HOME /config/addons/mongodb44/db
 
 /usr/local/bin/docker-entrypoint.sh mongod
