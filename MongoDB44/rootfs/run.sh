835b1955b2d000c292f2e114d5fa2bcdeabf12b9
DDanii <ddani1995@gmail.com>
2023-11-14 00:51:05 +0000

    export fix 2

--- MongoDB44/rootfs/run.sh
+++ MongoDB44/rootfs/run.sh
@@ -7,6 +7,6 @@ fi
 chown -R mongodb:mongodb /config/addons/mongodb44/db /config/addons/mongodb44/configdb
 usermod -d /config/addons/mongodb44/db mongodb
 
-export HOME /config/addons/mongodb44/db
+export HOME "/config/addons/mongodb44/db"
 
 /usr/local/bin/docker-entrypoint.sh mongod
