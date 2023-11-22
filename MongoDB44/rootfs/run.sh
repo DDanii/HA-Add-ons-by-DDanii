9fbc056f2ca322aed759e03fde51935e10296ab9
DDanii <ddani1995@gmail.com>
2023-11-22 13:17:31 +0000

    arg testing

--- MongoDB44/rootfs/run.sh
+++ MongoDB44/rootfs/run.sh
@@ -6,10 +6,10 @@ usermod -d /config/db mongodb
 
 export HOME=/config/db
 
-args="--dbpath /config/db"
+args=""
 
 if bashio::config.true 'smallfiles'; then
     args="${args} --smallfiles"
 fi
 
-/usr/local/bin/docker-entrypoint.sh mongod "${args}"
+/usr/local/bin/docker-entrypoint.sh mongod --dbpath /config/db "${args}"
