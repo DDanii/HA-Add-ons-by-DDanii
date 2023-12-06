0d51995c6b5bf361ee66a877592a1c1cc45148e7
DDanii <ddani1995@gmail.com>
2023-12-06 01:16:45 +0000

    mongo arg test2

--- MongoDB44/rootfs/run.sh
+++ MongoDB44/rootfs/run.sh
@@ -4,8 +4,6 @@ mkdir -p /config/db
 chown -R mongodb:mongodb /config/db
 usermod -d /config/db mongodb
 
-args="--dbpath /config/db" # --config /config/mongod.conf"
-
 journal_path="$(bashio::config 'journal_path')"
 
 if bashio::config.has_value 'journal_path'; then
@@ -14,4 +12,4 @@ if bashio::config.has_value 'journal_path'; then
     ln -s "$journal_path" /config/db/journal
 fi
 
-/usr/local/bin/docker-entrypoint.sh mongod "${args}"
+/usr/local/bin/docker-entrypoint.sh mongod --dbpath /config/db --config /config/mongod.conf
