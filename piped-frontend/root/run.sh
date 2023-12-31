cb7013eece13e6b362ec9421f9acc3f9857d3b9b
DDanii <ddani1995@gmail.com>
2023-12-31 23:37:10 +0000

    piped-frontend backend_url fix test

--- piped-frontend/root/run.sh
+++ piped-frontend/root/run.sh
@@ -4,4 +4,4 @@ export BACKEND_HOSTNAME
 
 BACKEND_HOSTNAME=$(bashio::config 'BACKEND_HOSTNAME')
 
-ash -c "sed -i s/pipedapi.kavin.rocks/$BACKEND_HOSTNAME/g /usr/share/nginx/html/assets/* && /docker-entrypoint.sh && nginx -g 'daemon off;'"
+ash -c "sed -i s@https://pipedapi.kavin.rocks@$BACKEND_HOSTNAME@g /usr/share/nginx/html/assets/* && /docker-entrypoint.sh && nginx -g 'daemon off;'"
