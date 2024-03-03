e903ebbad3b262c2971170ed55cf233297f7bb20
DDanii <ddani1995@gmail.com>
2024-03-03 04:27:59 +0000

    azure test without remap

--- azuracast/rootfs/run.sh
+++ azuracast/rootfs/run.sh
@@ -11,20 +11,20 @@ for KEY in "${arr[@]}"; do
     fi
 done
 
-mkdir -p /config/mysql
-rm -r /var/lib/mysql
-ln -s /config/mysl /var/lib/
+# mkdir -p /config/mysql
+# rm -r /var/lib/mysql
+# ln -s /config/mysl /var/lib/
 
-rm -r /var/azuracast/backups
-mkdir -p /config/backups
-ln -s /config/backups /var/azuracast/
+# rm -r /var/azuracast/backups
+# mkdir -p /config/backups
+# ln -s /config/backups /var/azuracast/
 
-rm -r /var/azuracast/stations
-mkdir -p /config/stations
-ln -s /config/stations /var/azuracast/
+# rm -r /var/azuracast/stations
+# mkdir -p /config/stations
+# ln -s /config/stations /var/azuracast/
 
-rm -r /var/azuracast/storage
-mkdir -p /data/storage
-ln -s /data/storage /var/azuracast/
+# rm -r /var/azuracast/storage
+# mkdir -p /data/storage
+# ln -s /data/storage /var/azuracast/
 
 /usr/bin/tini /usr/local/bin/my_init --no-main-command
