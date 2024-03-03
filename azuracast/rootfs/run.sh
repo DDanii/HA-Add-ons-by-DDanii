9d0212d427baddea9115c5a9d1d2f3089a85352d
DDanii <ddani1995@gmail.com>
2024-03-03 03:12:39 +0000

    more permission testing

--- azuracast/rootfs/run.sh
+++ azuracast/rootfs/run.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/bashio
 
-ADDTO DOCKERFILE
+mkdir -p /config/mysql
+rm -r /var/lib/mysql
+ln -s /config/mysl /var/lib/
 
-RUN apt-get update; \
-    apt-get install -y jq bash wget tar curl
+rm -r /var/azuracast/backups
+mkdir -p /config/backups
+ln -s /config/backups /var/azuracast/
 
-RUN mkdir -p /tmp/bashio && \
-    wget -q -O - "https://github.com/hassio-addons/bashio/archive/v0.14.3.tar.gz" | \
-    tar -xzf - --strip 1 -C /tmp/bashio && \
-    mv /tmp/bashio/lib /usr/lib/bashio && \
-    ln -s /usr/lib/bashio/bashio /usr/bin/bashio && \
-    rm -rf /tmp/bashio \
-    \
-    && wget -q -O /usr/bin/tempio \
-    "https://github.com/home-assistant/tempio/releases/download/2021.09.0/tempio_amd64" \
-    && chmod a+x /usr/bin/tempio
+rm -r /var/azuracast/stations
+mkdir -p /config/stations
+ln -s /config/stations /var/azuracast/
 
-COPY rootfs/ /
+rm -r /var/azuracast/storage
+mkdir -p /data/storage
+ln -s /data/storage /var/azuracast/
 
-RUN chmod a+x /run.sh
-
-CMD /run.sh
+/usr/bin/tini /usr/local/bin/my_init --no-main-command
