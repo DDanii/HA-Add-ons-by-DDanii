afbfa6e159bb96a9990f5fcfbc4f375864129dec
DDanii <ddani1995@gmail.com>
2025-07-31 13:37:40 +0000

    2fauth fix 5 test

--- 2fauth/rootfs/run.sh
+++ 2fauth/rootfs/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bashio
 
 JSONSOURCE="/data/options.json"
-mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+sudo mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
 for KEY in "${arr[@]}"; do
     if bashio::config.has_value "$KEY"; then
         VALUE=$(bashio::config "$KEY")
