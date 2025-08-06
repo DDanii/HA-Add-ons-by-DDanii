85881c732baefebf2e3d5b266c1c9693d2305acb
DDanii <ddani1995@gmail.com>
2025-08-06 21:49:16 +0000

    2fauth fix 16 test

--- 2fauth/rootfs/run.sh
+++ 2fauth/rootfs/run.sh
@@ -2,13 +2,15 @@
 
 echo "Started hassio setup"
 
-# JSONSOURCE="/data/options.json"
-# mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
-# for KEY in "${arr[@]}"; do
-#     if bashio::config.has_value "$KEY"; then
-#         VALUE=$(bashio::config "$KEY")
-#         export "$KEY=$VALUE"
-#     fi
-# done
+bashio::log.level 1
+
+JSONSOURCE="/data/options.json"
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+for KEY in "${arr[@]}"; do
+    if bashio::config.has_value "$KEY"; then
+        VALUE=$(bashio::config "$KEY")
+        export "$KEY=$VALUE"
+    fi
+done
 
 sudo -g \#1000 -u \#1000 exec /usr/local/bin/entrypoint.sh
