15a54e612c51c2211ff03dfb7013fc48aed4eb53
DDanii <ddani1995@gmail.com>
2025-08-08 19:54:18 +0000

    2fauth fix 25 test

---
+++ 2fauth/rootfs/ha_config_setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bashio
+
+echo "Started hassio setup"
+
+JSONSOURCE="/data/options.json"
+
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+for KEY in "${arr[@]}"; do
+    echo "$KEY"
+    if bashio::config.has_value "$KEY"; then
+        echo "has value"
+        VALUE=$(bashio::config "$KEY")
+        echo "$VALUE"
+        export "$KEY=$VALUE"
+    fi
+done
+#sudo -g \#1000 -u \#1000
+exec /usr/local/bin/entrypoint.sh
--- 2fauth/rootfs/run.sh
+++ 2fauth/rootfs/run.sh
@@ -1,18 +1,5 @@
-#!/usr/bin/bashio
+#!/bin/bash
 
-echo "Started hassio setup"
+unset LOG_LEVEL
 
-JSONSOURCE="/data/options.json"
-
-mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
-for KEY in "${arr[@]}"; do
-    echo "$KEY"
-    if bashio::config.has_value "$KEY"; then
-        echo "has value"
-        VALUE=$(bashio::config "$KEY")
-        echo "$VALUE"
-        export "$KEY=$VALUE"
-    fi
-done
-#sudo -g \#1000 -u \#1000
-exec /usr/local/bin/entrypoint.sh
+exec /ha_config_setup.sh
