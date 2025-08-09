d90866a38e22033d83e75ca6860e2e80d2980380
DDanii <ddani1995@gmail.com>
2025-08-09 15:52:04 +0000

    2fauth debug 28

--- 2fauth/rootfs/ha_config_setup.sh
+++ 2fauth/rootfs/ha_config_setup.sh
@@ -6,11 +6,8 @@ JSONSOURCE="/data/options.json"
 
 mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
 for KEY in "${arr[@]}"; do
-    echo "$KEY"
     if bashio::config.has_value "$KEY"; then
-        echo "has value"
         VALUE=$(bashio::config "$KEY")
-        echo "$VALUE"
         export "$KEY=$VALUE"
     fi
 done
--- 2fauth/rootfs/run.sh
+++ 2fauth/rootfs/run.sh
@@ -2,4 +2,6 @@
 
 unset LOG_LEVEL
 
+read -n 1 -s -r -p "Press any key to continue" 
+
 exec /ha_config_setup.sh
