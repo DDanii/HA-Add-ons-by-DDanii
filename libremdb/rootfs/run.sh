e20066a9ce97fe4b1af77e2aa6406f7910f6db1b
DDanii <ddani1995@gmail.com>
2025-09-15 23:15:10 +0000

    added anonymousoverflow libremdb wikimore

---
+++ libremdb/rootfs/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bashio
+
+JSONSOURCE="/data/options.json"
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+for KEY in "${arr[@]}"; do
+    if bashio::config.has_value "$KEY"; then
+        VALUE=$(bashio::config "$KEY")
+        export "$KEY=$VALUE"
+    fi
+done
+
+exec todo
+
+ADDTO DOCKERFILE
+
+RUN apt-get update; \
+    apt-get install -y jq bash wget tar curl
+
+RUN mkdir -p /tmp/bashio && \
+    wget -q -O - "https://github.com/hassio-addons/bashio/archive/v0.17.1.tar.gz" | \
+    tar -xzf - --strip 1 -C /tmp/bashio && \
+    mv /tmp/bashio/lib /usr/lib/bashio && \
+    ln -s /usr/lib/bashio/bashio /usr/bin/bashio && \
+    rm -rf /tmp/bashio \
+    \
+    && wget -q -O /usr/bin/tempio \
+    "https://github.com/home-assistant/tempio/releases/download/2021.09.0/tempio_amd64" \
+    && chmod a+x /usr/bin/tempio
+
+COPY rootfs/ /
+
+RUN chmod a+x /run.sh
+
+ENTRYPOINT /run.sh
