c596a16630a2013fb619faf023975326bd46539b
DDanii <ddani1995@gmail.com>
2025-09-16 19:29:52 +0000

    searxng switchable ingress compatibility test

---
+++ searxng/rootfs/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set_url=$(cat /data/options.json | grep set_base_url_for_ingress | cut -d: -f2 | xargs)
+
+if [ "$set_url" = "true" ]; then
+export SEARXNG_BASE_URL
+SEARXNG_BASE_URL="$(wget --header=Authorization: Bearer $SUPERVISOR_TOKEN \
+    http://supervisor/addons/$HOSTNAME/info \
+    | grep ingress_url | cut -d: -f2 | xargs | cut -d, -f1)"
+fi
+
+exec /usr/local/searxng/entrypoint.sh
