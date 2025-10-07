5acab1885f9b3b577be27f60a1d77737c9d1de81
DDanii <ddani1995@gmail.com>
2025-10-07 12:51:54 +0000

    added discount discovery addon

---
+++ discount-discovery/rootfs/custom.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#write your custom commands befor the exec command
+
+
+exec node .output/server/index.mjs
---
+++ discount-discovery/rootfs/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+CONFIGPATH="/config/custom.sh"
+
+if [ ! -f $CONFIGPATH ]; then
+    cp /custom.sh $CONFIGPATH
+fi
+
+chmod +x $CONFIGPATH
+
+exec $CONFIGPATH
