03428f78cb6286006dbbd1198b05967aa42fdd15
DDanii <ddani1995@gmail.com>
2025-11-22 20:12:30 +0000

    pelican fix test 8

--- pelican-panel/rootfs/custom.sh
+++ pelican-panel/rootfs/custom.sh
@@ -2,4 +2,4 @@
 #write your custom commands befor the exec command
 
 
-exec sudo -E -u www-data:www-data /bin/ash /entrypoint.sh supervisord -n -c /etc/supervisord.conf
+exec /bin/ash /entrypoint.sh supervisord -n -c /etc/supervisord.conf
