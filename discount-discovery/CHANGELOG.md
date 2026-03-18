57c774ecd7aa2f6bf8c93ba5e76fda9ca5da6e49
DDanii <ddani1995@gmail.com>
2026-03-18 16:14:37 +0000

    Discount discovery breaking change update

---
+++ discount-discovery/CHANGELOG.md
@@ -0,0 +1,4 @@
+## [0.100] - 2026-03-18
+
+This version changes database to external CouchDB
+If you don't already have one there is an addon for that in my repository
--- discount-discovery/rootfs/run.sh
+++ discount-discovery/rootfs/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+JSONSOURCE="/data/options.json"
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+for KEY in "${arr[@]}"; do
+    if bashio::config.has_value "$KEY"; then
+        VALUE=$(bashio::config "$KEY")
+        export "$KEY=$VALUE"
+    fi
+done
 
 CONFIGPATH="/config/custom.sh"
 
