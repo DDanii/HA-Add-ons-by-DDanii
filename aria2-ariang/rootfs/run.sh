b37250e4f4ee79897323393cf6072c3f145845da
DDanii <ddani1995@freemail.hu>
2026-07-16 15:25:59 +0200

    aria start script patch

--- aria2-ariang/rootfs/run.sh
+++ aria2-ariang/rootfs/run.sh
@@ -23,4 +23,37 @@ if [ ! -f $ARIACONFIGPATH ]; then
     cp /aria2.conf $ARIACONFIGPATH
 fi
 
+
+
+conf_path=/config
+
+while read -r name value; do 
+    echo "Content of $name is ${value//\"/}"; 
+    if [ $name == "dir" ]; then
+        data_path="${value//\"/}"
+    fi
+done < conf_path/aria2.conf 
+
+echo "$data_path"
+
+if [ -n "$RPC_SECRET" ]; then
+    sed -i '/^rpc-secret=/d' $conf_path/aria2.conf
+    printf '\nrpc-secret=%s\n' "${RPC_SECRET}" >>$conf_path/aria2.conf
+
+    if [ -n "$EMBED_RPC_SECRET" ]; then
+        echo "Embedding RPC secret into ariang Web UI"
+        RPC_SECRET_BASE64=$(echo -n "${RPC_SECRET}" | base64 -w 0)
+        sed -i 's,secret:"[^"]*",secret:"'"${RPC_SECRET_BASE64}"'",g' $ariang_js_path
+    fi
+fi
+
+touch $conf_path/aria2.session
+
+userid="$(id -u)" # 65534 - nobody, 0 - root
+groupid="$(id -g)"
+
+chown -R "$userid":"$groupid" $conf_path
+chown -R "$userid":"$groupid" $data_path
+
+
 exec $CONFIGPATH
